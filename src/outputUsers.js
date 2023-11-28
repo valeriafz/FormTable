3a15e218cb4c957083e7df5d68a98b193ada80d7
suichanwa <augustinnegura746@gmail.com>
2023-11-28 13:51:03 -0800

    done

--- src/outputUsers.js
+++ src/outputUsers.js
@@ -1,29 +1,5 @@
-// import json from './users.json' assert { type: 'json' };
-
-// document.addEventListener('DOMContentLoaded', () => {
-//   const tableBody = document.querySelector('#userTable tbody');
-
-//   json.users.forEach((user) => {
-//     const row = tableBody.insertRow();
-//     const userId = user.id || '';
-//     const idCell = row.insertCell();
-//     idCell.appendChild(document.createTextNode(userId));
-//     const propertiesOrder = ['lastname', 'firstname', 'age', 'phone', 'email', 'password', 'repeatPassword'];
-//     propertiesOrder.forEach((property) => {
-//       const cell = row.insertCell();
-//       cell.appendChild(document.createTextNode(user[property] || ''));
-//     });
-//   });
-// });
-
-// implemented this bs in ur code but kept it above
-
 import json from "./users.json" assert { type: "json" };
 
-const editForm = () => {
-  // asta e doar placeholder, deja inlocuieste cu functia de edit
-  alert("Edit button clicked!");
-};
 const createEditButton = () => {
   const editButton = document.createElement("button");
   editButton.textContent = "Edit";
@@ -63,3 +39,38 @@ document.addEventListener("DOMContentLoaded", () => {
     addRow(user);
   });
 });
+
+const editForm = (event) => {
+  const row = event.target.closest("tr");
+  const cells = row.cells;
+  
+  for (let i = 1; i < cells.length - 1; i++) {
+    const currentData = cells[i].textContent;
+    
+    const input = document.createElement("input");
+    input.type = "text";
+    input.value = currentData;
+    cells[i].innerHTML = "";
+    cells[i].appendChild(input);
+  }
+
+  const saveButton = document.createElement("button");
+  saveButton.textContent = "Save";
+  saveButton.type = "button";
+  saveButton.addEventListener("click", saveChanges.bind(null, row));
+  cells[cells.length - 1].innerHTML = "";
+  cells[cells.length - 1].appendChild(saveButton);
+};
+
+const saveChanges = (row) => {
+  const cells = row.cells;
+
+  for (let i = 1; i < cells.length - 1; i++) {
+    const inputValue = cells[i].querySelector("input").value;
+    cells[i].innerHTML = "";
+    cells[i].appendChild(document.createTextNode(inputValue));
+  }
+
+  cells[cells.length - 1].innerHTML = "";
+  cells[cells.length - 1].appendChild(createEditButton());
+};
